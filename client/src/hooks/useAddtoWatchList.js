295e8bd502e2e602531ad8420a0cf9279d31bf18
Ne0sky <99585944+Ne0sky@users.noreply.github.com>
2024-04-11 20:33:30 +0530

    update client

---
+++ client/src/hooks/useAddtoWatchList.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+
+const useAddToWatchList = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(false);
+
+    const addToWatchList = async (data) => {
+        setIsLoading(true);
+        setError(null);
+        setSuccess(false);
+        
+        try {
+            const cookies = new Cookies();
+            const token = cookies.get('token');
+
+            const response = await axios.post('http://localhost:3000/user/add-to-playlist', data, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `${token}`
+                },
+            });
+            console.log(response.data.message);
+
+            setSuccess(true);
+        } catch (error) {
+            console.error(error.response.data.message);
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return { addToWatchList, isLoading, error };
+};
+
+export default useAddToWatchList;
